1b4df667add4f0aca3e3ecd87439063b538ffa80
p2108951 <darkfireyo@gmail.com>
2024-11-06 16:59:19 +0100

    update

--- src/twig.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('./templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => false, // Vous pouvez spécifier un chemin de cache si nécessaire
-]);
-?>
