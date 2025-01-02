4d5797dcbb0ae0309880409ef2b218d09b01398b
Nadir Ayoub <zenkai0401@gmail.com>
2025-01-02 14:10:11 +0100

    bug fixes

--- src/Controller/Aide.php
+++ src/Controller/Aide.php
@@ -4,10 +4,10 @@ require_once(__DIR__ . '/../Model/model.php');
 
 $data = [
     'routes' => [
-        'bootstrap' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
-        'css' => '/path/to/your/css',
-        'images' => '/path/to/your/images',
-        'js' => '/path/to/your/js',
+        'bootstrap' => '../node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'css' => '../public/css',
+        'images' => '../public/images',
+        'js' => '../public/js',
     ],
     'current_page' => 'aide',
 ];
