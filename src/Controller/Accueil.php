a03baac1e4acb98eb0276714851a17e429df9942
Nadir Ayoub <zenkai0401@gmail.com>
2025-01-02 14:13:44 +0100

    bug fixes

--- src/Controller/Accueil.php
+++ src/Controller/Accueil.php
@@ -5,6 +5,7 @@ require_once(__DIR__ . '/../Model/model.php');
 // Passer les données et les routes dans un tableau
 $data = [
     'routes' => $routes,
+    'current_page' => 'home',
 ];
 
 // Retourner les données
